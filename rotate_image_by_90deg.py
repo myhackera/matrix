22b7136d472586cf5fd37bdff9b0905e8d56ab18
shivanshu shukla <69464466+myhackera@users.noreply.github.com>
2022-01-21 12:46:07 +0530

    Create rotate_image_by_90deg.py

---
+++ rotate_image_by_90deg.py
@@ -0,0 +1,23 @@
+class Solution:
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
+    def reverseCol(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            j = 0
+            k = n-1
+            while j<k:
+                matrix[i][j], matrix[i][k] = matrix[i][k], matrix[i][j]
+                j += 1 
+                k -= 1
+    
+    
+    def rotate(self, matrix: List[List[int]]) -> None:
+        self.transpose(matrix)
+        self.reverseCol(matrix)
+        return matrix
+        
